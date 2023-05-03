eef1647de2a7cb002801fe85ffe3b6e51d77069c
xiangcitoZ <pzxiang1@gmail.com>
2023-05-03 13:23:51 +0200

    x

--- ApiOAuthEmpleados/Controllers/EmpleadosController.cs
+++ ApiOAuthEmpleados/Controllers/EmpleadosController.cs
@@ -4,12 +4,14 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using NSwag.Annotations;
 using System.Security.Claims;
 
 namespace ApiOAuthEmpleados.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [OpenApiTag("EMPRESAS")]
     public class EmpleadosController : ControllerBase
     {
         private RepositoryEmpleados repo;
@@ -19,14 +21,38 @@ namespace ApiOAuthEmpleados.Controllers
             this.repo = repo;
         }
 
+        // GET: api/Empresa
+        /// <summary>
+        /// Obtiene el conjunto de empresas, tabla EMPRESAS.
+        /// </summary>
+        /// <remarks>
+        /// Método para devolver todas las empresas de la BBDD
+        /// </remarks>
+        /// <response code="200">OK. Devuelve el objeto solicitado.</response>  
+        /// /// <response code="401">Unathorized. No autorizado</response>    
         [HttpGet]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<List<Empleado>>> GetEmpleados()
         {
             return await this.repo.GetEmpleadosAsync();
         }
 
+
+        // GET: api/Empresa
+        /// <summary>
+        /// Obtiene el conjunto de empresas, tabla EMPRESAS.
+        /// </summary>
+        /// <remarks>
+        /// Método para devolver todas las empresas de la BBDD
+        /// </remarks>
+        /// <response code="200">OK. Devuelve el objeto solicitado.</response>  
+        /// /// <response code="404">Not Found</response>   
+
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Empleado>> FindEmpleado(int id)
         {
             return await this.repo.FindEmpleadoAsync(id);
--- ApiOAuthEmpleados/Program.cs
+++ ApiOAuthEmpleados/Program.cs
@@ -3,6 +3,9 @@ using ApiOAuthEmpleados.Helpers;
 using ApiOAuthEmpleados.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using NSwag.Generation.Processors.Security;
+using NSwag;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,34 +31,70 @@ builder.Services.AddDbContext<EmpleadosContext>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title="Api ", Version="v1"
-        , Description = "Seguridad Api"
 
-    });
+//builder.Services.AddSwaggerGen(options =>
+//{
+//    options.SwaggerDoc("v1", new OpenApiInfo
+//    {
+//        Title="Api ", Version="v1"
+//        , Description = "Seguridad Api"
+
+//    });
+//});
+
+builder.Services.AddOpenApiDocument(document =>
+{
+    document.Title = "Api OAuth Empleados";
+    document.Description = "Api Timers 2023. Ejemolo OAuth";
+    // CONFIGURAMOS LA SEGURIDAD JWT PARA SWAGGER,
+    // PERMITE AÑADIR EL TOKEN JWT A LA CABECERA.
+    document.AddSecurity("JWT", Enumerable.Empty<string>(),
+        new NSwag.OpenApiSecurityScheme
+        {
+            Type = OpenApiSecuritySchemeType.ApiKey,
+            Name = "Authorization",
+            In = OpenApiSecurityApiKeyLocation.Header,
+            Description = "Copia y pega el Token en el campo 'Value:' así: Bearer {Token JWT}."
+        }
+    );
+    document.OperationProcessors.Add(
+        new AspNetCoreOperationSecurityScopeProcessor("JWT"));
 });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseSwagger();
+
+
+app.UseOpenApi();
 app.UseSwaggerUI(options =>
 {
-    options.SwaggerEndpoint("/swagger/v1/swagger.json"
-        , "Api OAuth Empleados");
+    options.InjectStylesheet("/css/bootstrap.css");
+    options.InjectStylesheet("/css/material3x.css");
+    options.SwaggerEndpoint(
+        url: "/swagger/v1/swagger.json", name: "Api v1");
     options.RoutePrefix = "";
-}
+    options.DocExpansion(DocExpansion.None);
+});
 
-    );
+//app.UseSwagger();
+//app.UseSwaggerUI(options =>
+//{
+//    options.SwaggerEndpoint("/swagger/v1/swagger.json"
+//        , "Api OAuth Empleados");
+//    options.RoutePrefix = "";
+//}
+
+//   );
 //if (app.Environment.IsDevelopment())
 //{
    
 //}
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
